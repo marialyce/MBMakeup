3b453d2f4b3a32e057f25841ee4ecfbe617fe6fb
marialyce <128184917+marialyce@users.noreply.github.com>
2023-09-10 12:01:57 -0300

    Update mbmakeup.js

--- mbmakeup.js
+++ mbmakeup.js
@@ -1,1 +1,12 @@
+// Este é apenas um exemplo básico de JavaScript para interatividade futura.
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.comprar');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            alert('Produto adicionado ao carrinho!');
+        });
+    });
+});
 
